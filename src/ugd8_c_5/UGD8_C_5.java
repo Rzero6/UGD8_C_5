0f4d797f6648b479846a74d4015329b9c90af534
Reynoldssss <michaelreynoldk@gmail.com>
2023-04-28 10:11:17 +0700

    Merge origin/master

---
+++ src/ugd8_c_5/UGD8_C_5.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ugd8_c_5;
+
+//import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.util.ArrayList;
+/**
+  
+ *
+ * @author VICTUS
+    Reynold Kunarto             210711015   C
+    Febiola Ika Putri Wibowo    210711023   C
+ */
+public class UGD8_C_5 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Scanner scan = new Scanner (System.in);
+        // TODO code application logic here
+    }
+
+}
